1ecb9f11e3fba6ad44b20ecec0166a34d9ee5e7a
ReyhanHerdi <reyhanherdiyanto180603@students.unnes.ac.id>
2024-12-13 13:09:05 +0700

    team api controller

--- taskO-management/routes/api.php
+++ taskO-management/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\api\auth\UserController;
+use App\Http\Controllers\api\TeamController;
+use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +14,9 @@ Route::get('/user', function (Request $request) {
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/register', [UserController::class, 'store']);
 Route::put('/users/update/{id}', [UserController::class, 'update']);
+
+// Team
+Route::get('/teams', [TeamController::class, 'index']);
+Route::post('/teams', [TeamController::class, 'store']);
+Route::put('/teams/{id}', [TeamController::class, 'update']);
+Route::delete('/teams/{id}', [TeamController::class, 'delete']);
